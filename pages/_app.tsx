a38e135b892387376be2414fda72f0d16ab628c2
Alex Kam <kavimaluskam@gmail.com>
2021-12-02 19:12:25 +0100

    feat: add basic layout

---
+++ pages/_app.tsx
@@ -0,0 +1,14 @@
+import { AppProps } from "next/dist/shared/lib/router/router";
+import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
+import { theme } from "../styles/theme";
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <ChakraProvider theme={theme}>
+      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
+}
+
+export default MyApp;
