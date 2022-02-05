f5462f6e917a0226728193dd7cbd5d935557825e
Alex Kam <kavimaluskam@gmail.com>
2022-02-05 18:26:30 +0100

    Add external profile links for component

--- configs/siteMetadata.ts
+++ configs/siteMetadata.ts
@@ -3,24 +3,28 @@ const config = {
     name: "kavimaluskam",
     headline: "Hey, I'm Alex Kam 👋",
     avatar: "/avatar.jpeg",
-    descriptions: [
-      "👨🏻‍🔧 Data engineer",
-      "🎨 Frontend hobbyists ",
-      "🍵 Slow-paced Web3 learner and hodler",
-      "🇭🇰 From Hong Kong",
-    ],
+    descriptions: ["Data engineer. Web3, Gym, Manga, Music. From 🇭🇰"],
   },
   job: {
     company: "SoundCloud",
     avatar: "/soundcloud.jpeg",
     descriptions: [
-      "I have relocated to Berlin 🇩🇪 and started working in SoundCloud in Sep, 2021.",
-      "Different from my previous data platform team experience, I am now working in a cross-functional team focusing in Royalties and Insights.",
+      "Relocated to Berlin 🇩🇪 and working in SoundCloud in Sep, 2021.",
+      "Different from my previous data platform team experience, Now working on user facing product with Big Table and Go.",
     ],
   },
-  letterboxdRecentMovies: { headline: "Lately I've watched 🍿" },
-  spotifyTopArtists: { headline: "These days I'm listening to 🎧" },
-  okuclubBooks: { headline: "Currently I'm Reading 📚" },
+  letterboxdRecentMovies: {
+    headline: "Lately watched 🍿",
+    profile: "https://letterboxd.com/kavimaluskam/",
+  },
+  spotifyTopArtists: {
+    headline: "These days listening to 🎧",
+    profile: "https://open.spotify.com/user/kaviktopy",
+  },
+  okuclubBooks: {
+    headline: "Currently reading 📚",
+    profile: "https://oku.club/user/kavimaluskam",
+  },
 };
 
 export default config;
